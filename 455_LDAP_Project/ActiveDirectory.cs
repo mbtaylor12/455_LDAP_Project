83a542bb03cbcf7659a9f6a31813cab6e0567ac8
mbtaylor12 <Basshunter64!>
2018-04-24 12:19:31 -0400

    Added methods in ActiveDirectory.cs to validate user credentials and create an account.

---
+++ 455_LDAP_Project/ActiveDirectory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Web;
+
+namespace _455_LDAP_Project
+{
+    public class ActiveDirectory
+    {
+        Server sv = new Server();
+
+        public bool ValidateCredentials(string username, string password)
+        {
+            String domain = sv.getIP();
+
+            using (var context = new PrincipalContext(ContextType.Domain, domain))
+            {
+                return context.ValidateCredentials(username, password);
+            }
+        }
+
+        public bool CreateAccount(String firstName, String lastName, String password, String phoneNum, String username)
+        {
+            bool success = true;
+
+            try
+            {
+                using (var pc = new PrincipalContext(ContextType.Domain, sv.getIP(), sv.getGenericUsername(), sv.getGenericPassword()))
+                {
+                    using (var up = new UserPrincipal(pc))
+                    {
+                        up.SamAccountName = username; // Username
+                        up.EmailAddress = username + "@security.edu"; // Email
+                        up.SetPassword(password); // Password
+                        up.Surname = firstName; //firstname
+                        up.Name = firstName + " " + lastName; //full name
+                        up.GivenName = lastName; //lastname
+                        up.DisplayName = firstName + " " + lastName; //displayname
+                        up.VoiceTelephoneNumber = phoneNum; //Phone
+                        up.Enabled = true;
+                        up.Save();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                success = false;
+            }
+
+            return success;
+        }
+    }
+}
